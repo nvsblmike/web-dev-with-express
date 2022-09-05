b368f9b618ac0467fd76c328cb90258f3078b272
nvsblmike <soundboylesh77@gmail.com>
2022-09-05 13:40:19 +0100

    modularized fortune code

--- meadowlark/site/meadowlark.js
+++ meadowlark/site/meadowlark.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+var fortune = require('./lib/fortune.js');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -10,13 +12,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
-var fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-];
+
 
 
 
@@ -25,10 +21,8 @@ app.get('/', function(req, res) {
   res.render('home');
 });
 
-app.get('/about', function(req, res){
-  var randomFortune =
-    fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', { fortune: randomFortune });
+app.get('/about', function(req, res) {
+  res.render('about', { fortune: fortune.getFortune() } );
 });
 
 // 404 catch-all handler (middleware)
